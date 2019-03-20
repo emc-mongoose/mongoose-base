1e18653da6abe6e4e522870b7c4123d06edb58eb
Andrey Kurilov <andrey.kurilov@emc.com>
2019-03-20 10:12:25 +0300

    v4.2.1: duplicate cli args fix

--- src/test/java/com/emc/mongoose/base/config/el/RandomPathTest.java
+++ src/test/java/com/emc/mongoose/base/config/el/RandomPathTest.java
@@ -10,15 +10,14 @@ public class RandomPathTest {
 
 	@Test
 	public final void testRandomPath()
-	throws Exception {
+					throws Exception {
 		final var pathPattern = Pattern.compile("(/[0-9a-f]){1,2}");
 		try (
-			final var rndPathInput = CompositeExpressionInputBuilder
-				.newInstance()
-				.expression("/${path:random(16, 2)}")
-				.build()
-		) {
-			for(var i = 0; i < 1000; i ++) {
+						final var rndPathInput = CompositeExpressionInputBuilder
+										.newInstance()
+										.expression("/${path:random(16, 2)}")
+										.build()) {
+			for (var i = 0; i < 1000; i++) {
 				final var path = rndPathInput.get();
 				final var matcher = pathPattern.matcher(path);
 				assertTrue(path, matcher.find());
