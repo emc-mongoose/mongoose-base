20b8dc4a4e3dff37358465721df606c70d74e5a3
dlarge <dave@largent.io>
2024-11-27 15:10:30 +0000

    Service now needs to be AutoCloseable

--- src/main/java/com/emc/mongoose/base/svc/Service.java
+++ src/main/java/com/emc/mongoose/base/svc/Service.java
@@ -5,7 +5,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 /** Created by kurila on 07.05.14. A remote service which has a name for resolution by URI. */
-public interface Service extends AsyncRunnable, Remote {
+public interface Service extends AutoCloseable, AsyncRunnable, Remote {
 
 	int registryPort() throws RemoteException;
 
